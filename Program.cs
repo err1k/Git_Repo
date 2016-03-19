fa0348681730b84c55c265b3229487cb1fae22e9
er1k er1k <er1k er1k>
2016-03-19 15:23:19 +0100

    Program changed

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace TestRepoProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test Repo!");
+            Console.WriteLine("Change");
+            Console.ReadKey();
+        }
+    }
+}
